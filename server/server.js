6b5ef3d1cbd731ca4fb1e191fa014db458d860fd
abhishek <abhishekmullackal@gmail.com>
2022-07-24 18:58:09 +0530

    first-commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get("/api",(req,res)=>{
+    res.json({"users":["userOne","userTwo","UserThree"]})
+
+})
+
+app.listen(5000,()=>{
+    console.log("server started on port 5000")
+})
